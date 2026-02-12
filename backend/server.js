f72299779c03dfca8e94ad2489d46bb4cc6a960b
lakshmanandev <lakshmananm2580@gmail.com>
2026-02-12 09:19:48 +0530

    cors enable

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 import seedUsers from "./src/utils/seed.js";
+import cors from "cors";
+
 
 // connectDB();
 connectDB().then(seedUsers);
 
 const PORT = process.env.PORT || 5000;
+//cors enabled
+app.use(cors({
+  origin: [
+    "https://lakshmanandev-mini-crm-mern.vercel.app",
+    "http://localhost:3000"
+  ],
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.status(200).json({
